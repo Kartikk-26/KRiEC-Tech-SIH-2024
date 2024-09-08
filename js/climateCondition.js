c99abf7c8b67b555054a174b04a9aa52f4ce0b52
Kartikjain2626 <kartikjain2626@gmail.com>
2024-09-08 21:16:33 +0530

    Soil Mositure Commit

--- js/climateCondition.js
+++ js/climateCondition.js
@@ -1,82 +1,86 @@
-const container = document.querySelector(".container1");
-const search = document.querySelector(".search-box button");
-const weatherBox = document.querySelector(".weather-box");
-const weatherDetails = document.querySelector(".weather-details");
-const error404 = document.querySelector(".not-found");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardsDiv = document.querySelector(".weather-cards");
+const API_KEY = "5b3149e264bbc37e6b3b7eef02cb3920"; // API key for OpenWeatherMap API
 
-search.addEventListener("click", () => {
-    const APIKey = "92e4e88e94b0469aa0a7c6648d04d088";
-
-    const city = "udaipur"
-
-    if (city == "") {
-        return;
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if (index === 0) { // HTML for the main weather card
+        return `<div class="details">
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                </div>
+                <div class="icon">
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>${weatherItem.weather[0].description}</h6>
+                </div>`;
+    } else { // HTML for the other five day forecast card
+        return `<li class="card">
+                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                </li>`;
     }
+}
 
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
-    )
-        .then((Response) => Response.json())
-        .then((json) => {
-            if (json.cod == "404") {
-                container.style.height = "400px";
-                weatherBox.classList.remove("active");
-                weatherDetails.classList.remove("active");
-                error404.classList.add("active");
-                return;
+const getWeatherDetails = (cityName, latitude, longitude) => {
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+    fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
+        // Filter the forecasts to get only one forecast per day
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueForecastDays.includes(forecastDate)) {
+                return uniqueForecastDays.push(forecastDate);
             }
-
-            container.style.height = "555px";
-            weatherBox.classList.add("active");
-            weatherDetails.classList.add("active");
-            error404.classList.remove("active");
-
-            const image = document.querySelector(".weather-box img");
-            const temperature = document.querySelector(
-                ".weather-box .temperature"
-            );
-            const description = document.querySelector(
-                ".weather-box .description"
-            );
-            const humidity = document.querySelector(
-                ".weather-details .humidity span"
-            );
-            const wind = document.querySelector(".weather-details .wind span");
-
-            switch (json.weather[0].main) {
-                case "Clear":
-                    image.src = "image/clear.png";
-                    break;
-
-                case "Rain":
-                    image.src = "image/rain.png";
-                    break;
-
-                case "Snow":
-                    image.src = "image/snow.png";
-                    break;
-
-                case "Clouds":
-                    image.src = "image/cloud.png";
-                    break;
-
-                case "Mist":
-                    image.src = "image/mist.png";
-                    break;
-
-                case "Haze":
-                    image.src = "image/mist.png";
-                    break;
-
-                default:
-                    image.src = "image/cloud.png";
-            }
-
-            temperature.innerHTML = `${parseInt(
-                json.main.temp
-            )}<span>°C</span>`;
-            description.innerHTML = `${json.weather[0].description}`;
-            humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}km/h`;
         });
-});
+
+        // Clearing previous weather data
+        currentWeatherDiv.innerHTML = "";
+        weatherCardsDiv.innerHTML = "";
+
+        // Creating weather cards and adding them to the DOM
+        fiveDaysForecast.forEach((weatherItem, index) => {
+            const html = createWeatherCard(cityName, weatherItem, index);
+            if (index === 0) {
+                currentWeatherDiv.insertAdjacentHTML("beforeend", html);
+            } else {
+                weatherCardsDiv.insertAdjacentHTML("beforeend", html);
+            }
+        });
+    }).catch(() => {
+        alert("An error occurred while fetching the weather forecast!");
+    });
+}
+
+const searchByCityName = (cityName) => {
+    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
+    // Get entered city coordinates (latitude, longitude, and name) from the API response
+    fetch(API_URL)
+        .then((response) => response.json())
+        .then((data) => {
+            if (!data.length) return alert(`No coordinates found for ${cityName}`);
+            const { lat, lon, name } = data[0];
+            getWeatherDetails(name, lat, lon);
+        })
+        .catch(() => {
+            alert("An error occurred while fetching the coordinates!");
+        });
+};
+
+//Get Reference to our Elements
+let number = document.getElementById('number');
+let counter =0;
+
+setInterval(()=>{
+    if (counter == 65) {
+        clearInterval;
+    }else{
+        counter +=1;
+        number.innerHTML =  `${counter}%`; 
+    }
+},30)
