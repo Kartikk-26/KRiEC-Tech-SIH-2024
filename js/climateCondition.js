e511ebb5086dd00ac8a997033f203303705c1a22
Medhavi2025 <mkaushik2025@gmail.com>
2024-09-08 21:47:08 +0530

    circlecommit

--- js/climateCondition.js
+++ js/climateCondition.js
@@ -80,3 +80,16 @@ search.addEventListener("click", () => {
             wind.innerHTML = `${parseInt(json.wind.speed)}km/h`;
         });
 });
+
+//Get Reference to our Elements
+let number = document.getElementById('number');
+let counter =0;
+
+setInterval(()=>{
+    if (counter == 65) {
+        clearInterval;
+    }else{
+        counter +=1;
+        number.innerHTML =  `${counter}%`; 
+    }
+},30)
